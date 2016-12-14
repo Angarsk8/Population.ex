1ec0a7181ceee40e112e14e09e62e4ede52e43f6
Angarsk8 <andresa.garciah621@gmail.com>
2016-12-13 21:40:55 -0500

    bump version 0.1.1 ~> 0.1.2

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Population.Mixfile do
 
   def project do
     [app: :population,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
